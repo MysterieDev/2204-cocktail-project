5781184b9637c717854e4a63d706bc7cae07a85a
slezakIT <apps@slezak-it.de>
2025-04-22 11:49:41 +0200

    unique favorite entries work now

--- src/index.ts
+++ src/index.ts
@@ -9,36 +9,43 @@ let favorite_cocktails: Cocktail[] = [];
 // APP STATE - END
 
 
-function getCocktails(){
+function getCocktails() {
     return fetch("http://localhost:3000/drinks")
-    .then(res => res.json())
-    .then( (cocktails: Cocktail[])=> cocktails)
+        .then(res => res.json())
+        .then((cocktails: Cocktail[]) => cocktails)
 }
 
-async function showCocktail(){
+async function showCocktail() {
     const cocktails = await getCocktails();
-    const randomCocktailIndex = Math.floor(Math.random()*cocktails.length);
+    const randomCocktailIndex = Math.floor(Math.random() * cocktails.length);
     currentCocktail = cocktails[randomCocktailIndex];
     // render a random Cocktail 
     COCKTAIL_NAME_EL.innerHTML = currentCocktail.strDrink;
     COCKTAIL_IMAGE_EL.src = currentCocktail.strDrinkThumb;
 }
 
-function renderFavorites(){
+function renderFavorites() {
     // reset HTML
     FAVORITES_DIV_EL.innerHTML = "";
     favorite_cocktails.forEach(cocktail => {
-       const SINGLE_FAVORITE_ENTRY = document.createElement("DIV");
-       SINGLE_FAVORITE_ENTRY.innerHTML = `
+        const SINGLE_FAVORITE_ENTRY = document.createElement("DIV");
+        SINGLE_FAVORITE_ENTRY.innerHTML = `
        <p>${cocktail.strDrink}</p>
        <img src="${cocktail.strDrinkThumb}" style="width:50px" />
        `
 
-       FAVORITES_DIV_EL.appendChild(SINGLE_FAVORITE_ENTRY);
+        FAVORITES_DIV_EL.appendChild(SINGLE_FAVORITE_ENTRY);
     })
 }
 
-function addToFavorites(){
+function addToFavorites(idDrink: string) {
+
+    const isAlreadyTagged = favorite_cocktails.find(cocktail => cocktail.idDrink === idDrink);
+
+    if (isAlreadyTagged) {
+        return; // terminiert die Funktion
+    }
+
     // new entry to favorites
     favorite_cocktails.push(currentCocktail);
     // synchronize Localstorage
@@ -46,9 +53,9 @@ function addToFavorites(){
     renderFavorites();
 }
 
-function startApp(){
+function startApp() {
     COCKTAIL_BTN!.addEventListener("click", showCocktail);
-    COCKTAIL_FAVORITE_BTN.addEventListener("click", addToFavorites);
+    COCKTAIL_FAVORITE_BTN.addEventListener("click", () => addToFavorites(currentCocktail.idDrink));
     showCocktail();
     favorite_cocktails = getLocalstorageCocktails();
     renderFavorites();
